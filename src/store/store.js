8fef511f3a01d6b16cee9d1ec4f6bc2b1cb5c631
radhayo <romain.cianfarani@hotmail.fr>
2020-09-01 17:55:33 +0200

    95 Redux Thunk

--- src/store/store.js
+++ src/store/store.js
@@ -4,13 +4,13 @@ import rootReducer from "./reducer";
 export default createStore(rootReducer);*/
 
 
-
+import thunk from "redux-thunk"
 import React from "react"
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from "./reducer";
 import { Provider } from "react-redux";
 import initialState from "./initialState";
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, applyMiddleware(thunk));
 
 export default props => <Provider store={store} {...props}/>;
